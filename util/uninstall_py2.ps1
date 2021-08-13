678773bd1f17ff4577004c83db9946a44eaa8b82
Tim Y Chang <tim_y_chang@trendmicro.com>
2021-08-13 10:28:44 +0800

    DS-65284 Bugfix: remove py2 PATH

--- util/uninstall_py2.ps1
+++ util/uninstall_py2.ps1
@@ -15,6 +15,9 @@ try {
     $py2 = "C:\Python27"
     $py2_script = "C:\Python27\Scripts"
     $path = ($path.Split(';') | Where-Object { $_ -ne $py2 -and $_ -ne $py2_script}) -join ';'
+    $py2 = "C:\Python27\"
+    $py2_script = "C:\Python27\Scripts\"
+    $path = ($path.Split(';') | Where-Object { $_ -ne $py2 -and $_ -ne $py2_script}) -join ';'
     # Step 3. Set 
     [System.Environment]::SetEnvironmentVariable('PATH', $path, 'Machine') 
 }
